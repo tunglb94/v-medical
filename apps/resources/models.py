63dfb93a64b3644b0e756a9233ecc17ba4523cfc
macos <macos@macoss-MacBook-Pro.local>
2025-12-03 16:13:59 +0700

    Update base.html with dark modes

--- apps/resources/models.py
+++ apps/resources/models.py
@@ -10,11 +10,14 @@ class ProductDocument(models.Model):
     title = models.CharField(max_length=200, verbose_name="Tên tài liệu")
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='MACHINE')
     
-    # SỬA DÒNG NÀY: Dùng URLField để lưu link ảnh từ mạng
+    # Link ảnh online (để dùng được script tự động)
     image_url = models.URLField(max_length=500, blank=True, null=True, verbose_name="Link Ảnh minh họa")
     
     content = models.TextField(verbose_name="Nội dung chi tiết")
+    
     created_at = models.DateTimeField(auto_now_add=True)
+    # THÊM LẠI DÒNG NÀY ĐỂ KHỚP VỚI ADMIN
+    updated_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
         return self.title
